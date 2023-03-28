47205fe0e19a8ff871fa823bb7fd020f35d2052b
tkulpa <tomasz.kulpa.praca@gmail.com>
2023-03-28 17:57:04 +0200

    Add VERCEL_URL env var to nextauth conf

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,10 +6,12 @@ const scopes = ['identify', 'email', 'guilds.members.read'].join(' ')
 const providers = []
 
 if (!!process?.env?.DISCORD_CLIENT_ID && !!process?.env?.DISCORD_CLIENT_SECRET) {
+  const redirectUriPostfix = '/api/auth/callback/discord'
+  const redirectUri = process?.env?.VERCEL_URL ? `https://${process?.env?.VERCEL_URL}` : 'http://localhost:3000'
   providers.push(DiscordProvider({
     clientId: process?.env?.DISCORD_CLIENT_ID,
     clientSecret: process?.env?.DISCORD_CLIENT_SECRET,
-    authorization: { params: { scope: scopes } },
+    authorization: { params: { scope: scopes, redirect_uri: redirectUri + redirectUriPostfix } },
   }))
 } else {
   console.error('Invalid configuration, there is not DISCORD_CLIENT_ID or DISCORD_CLIENT_SECRET secret setup')
